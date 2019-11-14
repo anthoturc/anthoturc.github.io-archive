890208c3716a7403617f2dea3f82c32a8db41f10
epeake <elijah.peake@gmail.com>
2019-11-14 00:07:38 -0500

    prompt user for channel

--- src/interface.sh
+++ src/interface.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+MAX_CHANNEL=125
+MIN_CHANNEL=0
+CHANNEL=127
+
+printf "Let\'s get started.\n\n\nWhat channel are we on? (0-125): "
+while [ $CHANNEL -lt $MIN_CHANNEL -o $CHANNEL -gt $MAX_CHANNEL ]; do
+	read CHANNEL
+	if  [ $CHANNEL -lt $MIN_CHANNEL -o $CHANNEL -gt $MAX_CHANNEL ]
+		then printf "Invalid input. What channel are we on? (0-125): "
+	fi
+done
+echo $CHANNEL
