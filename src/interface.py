f961ebb66bc014f42899adc4229ed4aa54522072
epeake <elijah.peake@gmail.com>
2019-11-15 17:15:27 -0500

    finally sending that data :)"

--- src/interface.py
+++ src/interface.py
@@ -17,7 +17,6 @@ BAUD_RATE=115200
 
 
 if __name__ == "__main__":
-    PORT = sys.argv[1]
 
     # init pipe and channel to out of bounds
     channel=-1
@@ -34,42 +33,23 @@ if __name__ == "__main__":
         address = int(input())
         if not (MIN_ADDRESS <= channel <= MAX_ADDRESS):
             print("Invalid input. Re-enter channel ({0}-{1}): ".format(MIN_ADDRESS, MAX_ADDRESS))
+
+    ser = serial.Serial()
+    ser.baudrate = BAUD_RATE
+    ser.port = sys.argv[1]
+    ser.open()
     
+    channel = channel.to_bytes(1, byteorder='little')
+    address = address.to_bytes(4, byteorder='little')
+
+    ser.write(channel)
+    ser.write(address)
     
-    print(channel)
-    print(address)
 
+    # total = 0
     
-
-
-# # Sending data to Arduino
-# # TODO: I have no clue why we need to send then reset, but we do...
-# printf "0\n" > $DEVICE_PATH$DEV_PORT
-# printf "0\n" > $DEVICE_PATH$DEV_PORT
-
-# printf "Please reset your board in the next 10 seconds:\n"
-# sleep 1
-# echo 10
-# sleep 1
-# echo 9
-# sleep 1
-# echo 8
-# sleep 1
-# echo 7
-# sleep 1
-# echo 6
-# sleep 1
-# echo 5
-# sleep 1
-# echo 4
-# sleep 1
-# echo 3
-# sleep 1
-# echo 2
-# sleep 1
-# echo 1
-
-# # we have to reverse the address and channel for readInput()
-# # to understand why, see comment in readInput()
-# printf "$CHANNEL\n" | rev > $DEVICE_PATH$DEV_PORT
-# printf "$ADDRESS\n" | rev > $DEVICE_PATH$DEV_PORT
+    # while total < len(values):
+    #     print ord(ser.read(1))
+    #     total=total+1
+    
+    ser.close()
--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,17 @@ void readInput(void);
 void printConfig(void);
 
 /* Communication configuration variables */
-volatile bool is_configured = false;  // turns true only after both channel_sent & address_sent
-volatile bool channel_sent = false;
-volatile bool address_sent = false;
+volatile uint8_t sent_config_bytes = 0;  
 volatile uint8_t input_channel = 0;
-volatile uint32_t input_address = 0;
-volatile uint32_t place_val = 1;
-
-bool is_config_printed = false;
+volatile address input_address;
+volatile uint8_t * p_input_address;
+volatile bool is_config_printed = false;
 
 
 void setup() {
   Serial.begin(115200);
   SPI.begin();
+  p_input_address = input_address.bytes;
 
   /* Antenna is active low */
   pinMode(A2, PULLUP);
@@ -29,10 +27,10 @@ void setup() {
 }
 
 void loop() {
-  while ((!is_configured) && Serial.available()) {
+  while ((sent_config_bytes != 5) && Serial.available()) {
     readInput();
   }
-  if (is_configured && (!is_config_printed)) {
+  if ((sent_config_bytes == 5) && (!is_config_printed)) {
     printConfig();
     is_config_printed = true;
   }
@@ -51,48 +49,21 @@ void ISR_Antenna() {
  * and address settings for transmission.
  * 
  * Note that channel must be sent first over transmission, then address.  This is taken
- * into account in interfase.sh script.
+ * into account in interface.sh script.
  * 
  * Also note that each transmission must end in a new line character.  This is how we
  * can differrentiate between the channel and address transmissions.
- * 
- * Finally, note that the address and channel must be sent in reverse numerical order 
- * so we can properly handle the place vales of each of our digits.
- * 
  */
 void readInput() {
-  char currChar = (char) (Serial.read());
-    
-  /* signify the end of a transmission with \n */
-  if (currChar == '\n') {
-    if (!channel_sent) {
-      channel_sent = true;
-      place_val = 1;
-    } else if (!address_sent) {
-      address_sent = true;
-      is_configured = true;
-      place_val = 1;
-    }
-  } else {
-    /* 
-    * channel is input in reverse character order because
-    * we know how to increment the number with each digit
-    * when starting at the one's digit, but from the left
-    * we are completely uncsure of the place value.  This
-    * is taken care of in the bash script.
-    */
-    if (!channel_sent) {
-
-      /* converting ASCII character to correct integer */
-      input_channel += (place_val * (uint8_t) (currChar - '0'));
-      place_val *= 10;  // increment the place value with each digit
-    } else if (!address_sent) {
-
-      /* converting ASCII character to correct integer */
-      input_address += (place_val * (uint32_t) (currChar - '0'));
-      place_val *= 10;  // increment the place value with each digit
-    }
-  }   
+  uint8_t currByte = (uint8_t) (Serial.read());
+  
+  if (sent_config_bytes == 0) {
+    input_channel = currByte;
+  } else { // (sent_config_bytes < 5)
+    *p_input_address = currByte;
+    p_input_address++;
+  }
+  sent_config_bytes++;   
 }
  
 
@@ -104,5 +75,5 @@ void printConfig() {
   Serial.print("Selected channel: ");
   Serial.println(input_channel);
   Serial.print("Selected address: ");
-  Serial.println(input_address);
+  Serial.println(input_address.num);
 }
