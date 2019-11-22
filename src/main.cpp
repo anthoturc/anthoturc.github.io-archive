dfffad95d06fb52d40895eb13be7f83372cbc3a7
epeake <elijah.peake@gmail.com>
2019-11-21 23:51:41 -0500

    sends file extension

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #define FLUSH_COUNT 5  // number of sequential FLUSH_CONST needed to switch serial_state_e to ACTIVE
 #define CHANNEL_BYTES 1  // only 126 possible channels so use 1 byte
 #define ADDRESS_BYTES 4  // address width
+#define EXTENSION_BYTES 10 // expected bytes in our file extension
 
 /* prototypes */
 void ISR_Antenna(void);
 void readConfig(void);
 void printConfig(void);
+void sendFile(void);
+void transmit(volatile char);
 
 /*
  * States signify whether the board is currently being configured or 
@@ -38,13 +41,16 @@ volatile serial_state_e serial_state = FLUSHING;
 volatile uint8_t sent_config_bytes = 0;  
 volatile uint8_t input_channel = 0;
 volatile address input_address;
-volatile uint8_t * p_input_address;
+volatile uint8_t * p_input_address = input_address.bytes;
 volatile bool is_config_printed = false;
 volatile uint8_t serial_flush_count = 0;
+volatile char file_extension[EXTENSION_BYTES];
+volatile char * p_file_extension = file_extension;
+volatile bool fake_transmit = false;
+volatile uint8_t sent_transmit_bytes = 0; 
 
 void setup() {
   SPI.begin();
-  p_input_address = input_address.bytes;
 
   /* Antenna is active low */
   pinMode(A2, PULLUP);
@@ -66,14 +72,19 @@ void loop() {
       is_config_printed = true;
     }
 
-    // also our nRF needs to be receiving
     while (Serial.available()) {
-      volatile char stored = (char) (Serial.read());
-      if (stored == '\t') {
-        Serial.print("ready"); // now transmit
-        break;
+      volatile char curr_char = (char) (Serial.read());
+      if (!fake_transmit) {
+        if (curr_char == '\t') {
+          Serial.print("\t"); // now transmit
+          fake_transmit = true;
+          break;
+        }
+      } else {
+        transmit(curr_char);
       }
     }
+
     break;
   
   default:
@@ -90,25 +101,22 @@ void ISR_Antenna() {
 
 
 /*
- * Function readInput reads data sent over serial to configure the desired channel
+ * Function readConfig reads data sent over serial to configure the desired channel
  * and address settings for transmission.
  * 
  * Note that channel must be sent first over transmission, then address.  This is taken
- * into account in interface.sh script.
- * 
- * Also note that each transmission must end in a new line character.  This is how we
- * can differrentiate between the channel and address transmissions.
+ * into account in config.py script.
  */
 void readConfig() {
-  uint8_t currByte;
+  uint8_t curr_byte;
   
   switch (serial_state) {
   case FLUSHING:
 
     /* Flush until read FLUSH_COUNT FLUSH_CONST in a row */
     while (true) {
-      currByte = (uint8_t) (Serial.read());
-      if (currByte == FLUSH_CONST) {
+      curr_byte = (uint8_t) (Serial.read());
+      if (curr_byte == FLUSH_CONST) {
         serial_flush_count++;
       } else {
         serial_flush_count = 0;
@@ -122,11 +130,11 @@ void readConfig() {
     break;
   
   case ACTIVE:
-    currByte = (uint8_t) (Serial.read());
+    curr_byte = (uint8_t) (Serial.read());
     if (sent_config_bytes < CHANNEL_BYTES) {
-      input_channel = currByte;
+      input_channel = curr_byte;
     } else {
-      *p_input_address = currByte;
+      *p_input_address = curr_byte;
       p_input_address++;
     } 
     
@@ -139,6 +147,39 @@ void readConfig() {
   default:
     break;
   }   
+}
+
+
+/*
+ * Function transmit reads data sent over serial to configure the desired file
+ * extension so we can decode our sent hex file.
+ * 
+ * Note that the extension must be sent first over transmission, then the data.  
+ * This is taken into account in send_hex.py script.
+ * 
+ * We will send hex from the Arduino to the transmitter in chunks of 16 bytes.
+ * Afterwards, we will send a confirmation back to the computer so that the computer
+ * knows to send the next 16 bytes.  This is done because we may not be able to fit
+ * our entire file to send in memory.
+ */
+void transmit(volatile char curr_char) {
+  if (sent_transmit_bytes < EXTENSION_BYTES) {
+    *p_file_extension = curr_char;
+    p_file_extension++;
+  } else {
+    sendFile();
+  } 
+  
+  if (sent_transmit_bytes == EXTENSION_BYTES-1) {
+    fake_transmit = false;
+  }
+  sent_transmit_bytes++; 
+}
+
+void sendFile() {
+  for (auto i {0}; i < EXTENSION_BYTES; ++i) {
+    Serial.print(file_extension[i]);
+  }
 }
  
 
--- src/send_hex.py
+++ src/send_hex.py
@@ -4,7 +4,12 @@ import sys
 import serial
 
 # BAUD for serial communication
-BAUD_RATE=115200
+BAUD_RATE = 115200
+
+# Bit to send to Arduino to signal that we are about to send a file
+TRANSMISSION_BIT = 9
+
+EXTENSION_LEN = 10
 
 
 if __name__ == "__main__":
@@ -15,25 +20,38 @@ if __name__ == "__main__":
     
     print(sys.argv)
     # send over file extension for decoding
-    file_extension = bytearray()
-    raw_hex = bytearray()
-    transmit = bytearray()
+    file_extension_bytes = bytearray()
+
+    # take everything after the .
+    file_extension = []
+    file_extension.extend(map(ord, sys.argv[3].split('.')[1]))
+
+    # fill in unised chars with spaces
+    space_pad = [ord(" ") for _ in range(EXTENSION_LEN - len(file_extension))]
+    file_extension.extend(space_pad)
+    
+    
+    raw_hex_bytes = bytearray()
+    transmit_bytes = bytearray()
 
     # ::-1 because little endian
-    file_extension.extend(map(ord, sys.argv[3].split('.')[1][::-1]))   # take everything after the .
-    raw_hex.extend(map(ord, sys.argv[2][::-1]))  # TODO: send over size of hex as checksum
-    transmit.extend([9])
+    file_extension_bytes.extend(file_extension)
+    raw_hex_bytes.extend(map(ord, sys.argv[2]))  # TODO: send over size of hex as checksum
+    transmit_bytes.extend([TRANSMISSION_BIT])
 
-    ser.write(transmit) # switch to transmit state
+    ser.write(transmit_bytes) # switch to transmit state
     while True:
         config_string = ""
         while ser.in_waiting:
             config_string += ser.read(1).decode("utf-8") # check that arduino confirms
             print(config_string)
-        if config_string == "ready":
+            if config_string == "\t":
+                break
+
+        if config_string == "\t":
             break
 
-    ser.write(file_extension)
-    ser.write(raw_hex)
+    ser.write(file_extension_bytes)
+    # ser.write(raw_hex_bytes)
     
     ser.close()
